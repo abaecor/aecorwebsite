225d352cf59671ac1c423392db3ead77c816242e
abhardwaj <abhardwaj@aecortech.com>
2013-04-01 19:57:56 +0530

    changes

--- index.php
+++ index.php
@@ -37,17 +37,19 @@
 <!-- Nav Start -->
 <header>
             <nav style='position: fixed; z-index: 1;'>
+           <div class="menu-wrapper">
             <a class='logo' href="#home"></a>
-            <ul>
-                <li class='first'><a href="#home" title='home'><span>Home</span></a></li>
-                <li><a class="responsive_text" href="#help" title="help">How We Can Help</a></li>
-                <li><a class="responsive_text" href="#about" title="about">About Us</a></li>
-                <li><a class="responsive_text" href="#meettheteam" title="meettheteam">Meet The Team</a></li>
-                <li><a class="responsive_text" href="#work" title="work">Work</a></li>
-                <li><a class="responsive_text" href="#contact" title="contact">Contact Us</a></li>
+            <ul class="main-navi">
+                <li><a href="#help" title="help">How we can help</a></li>
+                <li><a href="#about" title="about">About us</a></li>
+                <li><a href="#meettheteam" title="meettheteam">Meet the team</a></li>
+                <li><a href="#work" title="work">Work</a></li>
+                <li><a class="responsive_text" href="#contact" title="contact">Contact us</a></li>
             </ul>
+            </div>
             <!--<select><option value="">Go to...</option><option value="#home">Home</option><option value="#services">Services</option><option value="#about">About Us</option><option value="#portfolio">Portfolio</option><option value="#contact">Contact Us</option></select>
-            </nav>-->
+            -->
+            </nav>
 </header>            
 <!-- Nav End -->
 
@@ -67,11 +69,11 @@
                 <br/>
             </h3>
             <div class='bottom'>
-                <a class='btn' href='#help'> How We Can Help</a><br/>
-                <a class='banner_nav' href="#about" title="about">About Us </a> |
-                <a class='banner_nav' href="#meettheteam" title="meettheteam">Meet The Team </a> |
+                <a class='btn banner-btn' href='#help'> How we can help</a><br/>
+                <a class='banner_nav' href="#about" title="about">About us </a> |
+                <a class='banner_nav' href="#meettheteam" title="meettheteam">Meet the team </a> |
                 <a class='banner_nav' href="#work" title="work">Work </a> |
-                <a class='banner_nav' href="#contact" title="contact">Contact Us </a>
+                <a class='banner_nav' href="#contact" title="contact">Contact us </a>
             </div>
         </div>   
     </div>
@@ -84,7 +86,7 @@
     <!--<div class='mid_head'></div>  !-->
     <div class="scrollvspace"></div> 
     <div class="span12">
-                <h1 class="responsive_text">How We Can Help</span></h1>
+                <h1 class="responsive_text help-how">How We Can Help</span></h1>
                 <h3>We offer the whole kit and caboodle! From process review, flow charts and site</br>
                     mapping - to design, coding and infrastructure. Wow! </h3>
         </div> 
@@ -122,7 +124,7 @@
                 <a href="#"><span class="serv_icon"><img src="images/icon3.png" alt="img" /></span></a>
                 <article class="content">
                     <span class="strip"></span>
-                    <h4>Mobile Web & Apps</h4>
+                    <h4>Mobile Web &amp; Apps</h4>
                     <p>iOS, Android, Phonegap, HTML5,<br/>
                         jQuery Mobile, Bootstrap, XML,<br/>
                         JSON, APIs.<br/>
@@ -151,7 +153,7 @@
                 <a href="#"><span class="serv_icon"><img src="images/icon5.png" alt="img" /></span></a>
                 <article class="content">
                     <span class="strip"></span>
-                    <h4>Hosting & Infrastructure</h4>
+                    <h4>Hosting &amp; Infrastructure</h4>
                     <p>Cloud, migration, sys admin,<br/>
                         network monitoring, AWS<br/>
                         Cloudwatch, Rackspace, Apache,<br/>
@@ -183,12 +185,12 @@
 <article class="container">
     <div class="row">
         <div class="span12">
-            <h1 class="responsive_text">Meet The Team</span></h1>
+            <h1 class="responsive_text help-how">Meet the team</span></h1>
             <img id='grp' src='images/group1.png' width='100%'/>
             <div class='div1'></div>
             <div class="div2"></div>
             <div class="div3"></div>
-            <div class="div4" onmouseout="document.getElementById('grp').src='images/group1.png';" onmouseover="document.getElementById('grp').src='images/group2.png'"></div>
+            <div class="div4" onmouseout="document.getElementById('grp').src='images/group1.png';" onmouseover="document.getElementById('grp').src='images/grouphover.png'"></div>
             <div class="div5"></div>
             <div class="div6"></div>
         </div>
@@ -209,8 +211,8 @@
     <div class="row">
         <div class="span12">
             <div class="sub_header">
-                <h1 class="responsive_text">About Us</h2>
-                <h3>Kiss. Embrace. Love.</h3>
+                <h1 class="responsive_text help-how">About us</h2>
+                <h3 class='about-us-head'>Kiss. Embrace. Evolve.</h3>
             </div>
     	</div>
 	</div>
@@ -222,7 +224,7 @@
 	        <img src="images/lip.png" alt="img" />
             <article class="content">
                 <span class="strip"></span>
-                <h4> Keep It Simple (stupid!) </h4>
+                <h4> Keep It Simple (Stupid!) </h4>
                 <p>Don't over complicate matters.<br/> Define the problem, <br/>set your goals and achieve them.</p>
             </article>
         </div>
@@ -230,7 +232,7 @@
             <img src="images/embrace.png" alt="img" />
             <article class="content">
                 <span class="strip"></span>
-                <h4> Embrace Technology </h4>
+                <h4> Embrace technology </h4>
                 <p>Don't be scared of what you<br/> don't know. Find out what’s<br/> going on, try things out, adapt<br/> and share ideas. </p>
             </article>
         </div>
@@ -240,7 +242,7 @@
                 <span class="strip"></span>
                 <h4>Evolve</h4>
                 <p>
-                     No body is perfect. Tweak, test<br/> and improve. Learn from<br/> mistakes and strive for<br/> perfection. 
+                     No body is perfect. Tweak, test<br/> and continually improve. Learn <br/>from mistakes and strive for<br/> perfection. 
                 </p>
             </article>
         </div>
@@ -256,8 +258,8 @@
     <div class="row red_banner">
         <div class="span12">
             <div class="sub_header">
-                <h1 class="responsive_text">Work</h2>
-                <h3>It's not all about design and making your website look nice. Check out
+                <h1 class="responsive_text work-txt">Work</h2>
+                <h3 class='work-subtxt'>It's not all about design and making your website look nice. Check out
                     <br/>some of the case studies below to see how we helped these customers. </h3>
             </div>
         </div>
@@ -267,11 +269,11 @@
             <img id='work1' src="images/pad1.png" alt="img" onmouseout="document.getElementById('work1').src='images/pad1.png';" onmouseover="document.getElementById('work1').src='images/pad1_hov.png'"/>
             <article class="content">
                 <span class="strip"></span>
-                <h4> Club Website </h4>
+                <h4> Club website </h4>
                 <p>
-                    Digital Strategy, Infrastructure,</br>
-                    Web Development, Mobile &</br>
-                    Social Media.</br>
+                    Digital strategy, Infrastructure,</br>
+                    Web development, Mobile &amp;</br>
+                    Social media.</br>
                 </p>
             </article>
         </div>
@@ -280,9 +282,9 @@
             <article class="content">
                 <span class="strip"></span>
                 <h4>Powerleague</h4>
-                <p>Digital Strategy, Legacy<br/>
-                    Migration, Design & Web<br/>
-                    Development. </p>
+                <p>Digital strategy, Legacy<br/>
+                    migration, Design &amp; Web<br/>
+                    development. </p>
             </article>
         </div>
         <div class="span4">
@@ -291,8 +293,8 @@
                 <span class="strip"></span>
                 <h4>Greenworld Holidays</h4>
                 <p>
-                    Design, Web Development,<br/>
-                    Infrastructure, Outsourcing &<br/>
+                    Design, Web development,<br/>
+                    Infrastructure, Outsourcing &amp;<br/>
                     Mobile. 
                 </p>
             </article>
@@ -311,13 +313,18 @@
     <div class="row">
         <div class="span12 black_banner">
             <div class="sub_header">
-            <h1 class="responsive_text">Contact Us</h2>
-            <h3>A global presence. Our technical development is carried out in India,
-                <br/>whilst our core design, client contact and project management is done in the UK.</h3>
+            <h1 class="responsive_text work-txt">Contact us</h2>
+            <h3 class='work-subtxt'>A global presence. Our core design, client contact and project
+                <br/>management is based in the UK whilst our technical development is carried out in India.
+               </h3>
             </div>
+            <a href="#myModal" role="button" data-toggle="modal">
             <img id='map1' src='images/britain.png' width="40%" onmouseout="document.getElementById('map1').src='images/britain.png';" onmouseover="document.getElementById('map1').src='images/brit_map.png'"/>
+            </a>
+            <a href="#myModalbaroda" role="button" data-toggle="modal">
             <img id='map2' src='images/india.png' width="40%" onmouseout="document.getElementById('map2').src='images/india.png';" onmouseover="document.getElementById('map2').src='images/india_map.png'"/>
-    	</div>
+            </a>
+        </div>
     </div>
 
     <div class='row'>
@@ -346,10 +353,10 @@
                         <label for='message'>Enter the code above here :</label><br>
                         <input id="6_letters_code" name="6_letters_code" type="text"><br>
                         <input type='hidden' id='session_captcha' value='<?php echo $_SESSION["6_letters_code"]?>'/>
-                        <small>Can't read the image? click <a href='javascript: refreshCaptcha();'>here</a> to refresh</small>
+                        <small>Can't read the image? click <a class='link' href='javascript: refreshCaptcha();'>here</a> to refresh</small>
                         </p>
 
-                        <p><input class="contact_button button" type='button' name="submit" id="submit" value="Email Us!" /></p>
+                        <p><input class="contact_button button" type='button' name="submit" id="submit" value="Email us!" /></p>
 
                     </div>
                     
@@ -407,5 +414,52 @@ function refreshCaptcha()
     img.src = img.src.substring(0,img.src.lastIndexOf("?"))+"?rand="+Math.random()*1000;
 }
 </script>
+<!-- Modal -->
+<div id="myModal" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  <div class="modal-body">
+    <iframe width="583" height="393" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=+London+Office+Barley+Mow+Centre+10+Barley+Mow+Passage+London,+W4+4PH+UK&amp;sll=37.0625,-95.677068&amp;sspn=46.677964,107.138672&amp;ie=UTF8&amp;hq=London+Office+Barley+Mow+Centre&amp;hnear=10+Barley+Mow+Passage,+London+W4+4PH,+United+Kingdom&amp;t=m&amp;fll=51.492391,-0.261502&amp;fspn=0.001127,0.00327&amp;st=105250506097979753968&amp;rq=1&amp;ev=zi&amp;split=1&amp;ll=51.492079,-0.261848&amp;spn=0.002625,0.006244&amp;z=17&amp;output=embed"></iframe><br /><small><a href="https://maps.google.com/maps?f=q&amp;source=embed&amp;hl=en&amp;geocode=&amp;q=+London+Office+Barley+Mow+Centre+10+Barley+Mow+Passage+London,+W4+4PH+UK&amp;sll=37.0625,-95.677068&amp;sspn=46.677964,107.138672&amp;ie=UTF8&amp;hq=London+Office+Barley+Mow+Centre&amp;hnear=10+Barley+Mow+Passage,+London+W4+4PH,+United+Kingdom&amp;t=m&amp;fll=51.492391,-0.261502&amp;fspn=0.001127,0.00327&amp;st=105250506097979753968&amp;rq=1&amp;ev=zi&amp;split=1&amp;ll=51.492079,-0.261848&amp;spn=0.002625,0.006244&amp;z=17" style="color:#0000FF;text-align:left">View Larger Map</a></small>
+  </div>
+  <div class="modal-footer">
+    <button class="btn" data-dismiss="modal" aria-hidden="true">Close</button>
+  </div>
+</div>
+
+<div id="myModalbaroda" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  <div class="modal-body">
+    <iframe width="583" height="393" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=Sayajigunj,+Vadodara,+Gujarat,+India&amp;aq=&amp;sll=51.492079,-0.261799&amp;sspn=0.001134,0.00327&amp;ie=UTF8&amp;hq=&amp;hnear=Sayajigunj,+Vadodara,+Gujarat,+India&amp;t=m&amp;ll=22.308155,73.183966&amp;spn=0.031207,0.049953&amp;z=14&amp;output=embed"></iframe><br /><small><a href="https://maps.google.com/maps?f=q&amp;source=embed&amp;hl=en&amp;geocode=&amp;q=Sayajigunj,+Vadodara,+Gujarat,+India&amp;aq=&amp;sll=51.492079,-0.261799&amp;sspn=0.001134,0.00327&amp;ie=UTF8&amp;hq=&amp;hnear=Sayajigunj,+Vadodara,+Gujarat,+India&amp;t=m&amp;ll=22.308155,73.183966&amp;spn=0.031207,0.049953&amp;z=14" style="color:#0000FF;text-align:left">View Larger Map</a></small>
+  </div>
+  <div class="modal-footer">
+    <button class="btn" data-dismiss="modal" aria-hidden="true">Close</button>
+  </div>
+ 
+</div>
+<div id="backtotop"><a href="#"><img src="images/back_to_top_btn.png" border="0" alt="Go to TOP" /></a></div>
+<!-- back to top script -->
+
+<!-- include jQuery Library -->
+<script type="text/javascript" src="js/jquery.js"></script>
+<!-- efforless script below -->
+<script type="text/javascript">
+    jQuery(document).ready(function(){
+        var pxShow = 300;//height on which the button will show
+        var fadeInTime = 1000;//how slow/fast you want the button to show
+        var fadeOutTime = 1000;//how slow/fast you want the button to hide
+        var scrollSpeed = 1000;//how slow/fast you want the button to scroll to top. can be a value, 'slow', 'normal' or 'fast'
+        jQuery(window).scroll(function(){
+            if(jQuery(window).scrollTop() >= pxShow){
+                jQuery("#backtotop").fadeIn(fadeInTime);
+            }else{
+                jQuery("#backtotop").fadeOut(fadeOutTime);
+            }
+        });
+         
+        jQuery('#backtotop a').click(function(){
+            jQuery('html, body').animate({scrollTop:0}, scrollSpeed); 
+            return false; 
+        }); 
+    });
+</script>
+<!-- lightbox end -->
 </body>
 </html>
+
